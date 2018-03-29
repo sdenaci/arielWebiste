565fabfdd2b1bdf123835b1f1f0489ca88896188
Sarah Denaci <sarahdenaci@gmail.com>
2018-03-29 14:46:23 -0400

    add working scroll

--- src/pages/research.js
+++ src/pages/research.js
@@ -5,7 +5,7 @@ export default () =>
   <div className='flexContainerCenter'>
   </div>
   <div className='flexContainer'>
-    <img src='https://www.fillmurray.com/300/300'/>
+    <div id='imager'/>
     <div className="researchBlurb">
       <h4>Bill Murray</h4>
       <p>He's an actor with a lot of stuff to his name. He also has a son who runs a bar and at the opening of this bar he poured a lot of shots, all of them either whiskey or tequila I forget which.</p>
@@ -14,5 +14,6 @@ export default () =>
   <div className='flexContainerCenter'>
     <p className='heading'>Publications</p>
   </div>
+  <p className='biblio'>Asexual Erotics: Intimate Readings of Compulsory Sexuality. Columbus: Ohio State University Press (Abnormativities Series, edited by Scott Herring). (Under Advanced Contract).</p>
   <p className='biblio'>Asexual Erotics: Intimate Readings of Compulsory Sexuality. Columbus: Ohio State University Press (Abnormativities Series, edited by Scott Herring). (Under Advanced Contract).</p>
 </div>
